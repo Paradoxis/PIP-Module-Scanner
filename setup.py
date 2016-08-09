e437b7c001258c9dd5f850b507dfe9ef1bf5e76c
Paradoxis <luke@paradoxis.nl>
2016-08-09 19:54:51 +0200

    Added setup and tag

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pip-module-scanner',
+    packages=['pip-module-scanner'],
+    version='0.1',
+    description='Scans your Python project for all installed third party pip libraries and generates a requirements.txt based output',
+    author='Luke Paris (Paradoxis)',
+    author_email='luke@paradoxis.nl',
+    url='https://github.com/Paradoxis/Python-Third-Party-Module-Scanner',
+    download_url='https://github.com/Paradoxis/Python-Third-Party-Module-Scanner/tarball/0.1',
+    keywords=['scanning', 'requirements.txt', 'requirements'],
+    classifiers=[],
+)
